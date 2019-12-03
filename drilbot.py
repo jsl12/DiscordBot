d9991bd811b10d0900c27f8dfb1ad7ad63915fac
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-12-02 22:15:36 -0600

    now sending a random dril tweet from the most recent 200

--- drilbot.py
+++ drilbot.py
@@ -1,6 +1,10 @@
-import discord
-import yaml
+import re
 from pathlib import Path
+from random import choice
+
+import discord
+import twitter
+import yaml
 
 apikey = Path('apikey.yaml')
 
@@ -8,6 +12,12 @@ with apikey.open('r') as file:
     key = yaml.load(file, Loader=yaml.SafeLoader)
 
 client = discord.Client()
+api = twitter.Api(
+    consumer_key=key['TWITTER']['API_KEY'],
+    consumer_secret=key['TWITTER']['API_SECRET_KEY'],
+    access_token_key=key['TWITTER']['ACCESS_TOKEN'],
+    access_token_secret=key['TWITTER']['ACCESS_TOKEN_SECRET']
+)
 
 @client.event
 async def on_ready():
@@ -15,11 +25,18 @@ async def on_ready():
 
 @client.event
 async def on_message(msg: discord.Message):
-    if msg.author == client.user:
+    if (msg.author == client.user) or (msg.channel.name != 'robotics-facility'):
         return
-    print(msg.channel)
-    print(msg.content)
-    if msg.author.display_name == 'Stream':
-        await msg.channel.send('shut the fcuk up')
+
+    if re.search('dril', msg.content):
+        await msg.channel.send(random_tweet('dril'))
+
+def random_tweet(screen_name, count=200):
+    return choice(api.GetUserTimeline(
+        screen_name=screen_name,
+        include_rts=False,
+        exclude_replies=True,
+        count=count
+    )).text
 
 client.run(key['DISCORD_TOKEN'])
