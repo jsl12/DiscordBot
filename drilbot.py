6b8bd07f5fe9f9db60dacd03b8ffce4c5f97c577
jsl12 <32917998+jsl12@users.noreply.github.com>
2019-12-02 22:37:24 -0600

    implemented keywords

--- drilbot.py
+++ drilbot.py
@@ -6,6 +6,8 @@ import discord
 import twitter
 import yaml
 
+KEYWORDS = ['dick', 'trol', 'ass', 'cum', 'smok']
+
 apikey = Path('apikey.yaml')
 
 with apikey.open('r') as file:
@@ -29,14 +31,31 @@ async def on_message(msg: discord.Message):
         return
 
     if re.search('dril', msg.content):
+        for k in KEYWORDS:
+            if k in msg.content:
+                text = recent_keyword_tweet('dril', keyword=k).text
+                if text is not None:
+                    await msg.channel.send(text)
+                else:
+                    break
+                return
         await msg.channel.send(random_tweet('dril'))
 
-def random_tweet(screen_name, count=200):
-    return choice(api.GetUserTimeline(
+def random_tweet(screen_name:str , count=200) -> str:
+    return choice(recent_tweets(screen_name, count)).text
+
+def recent_tweets(screen_name: str, count:int = 200):
+    return api.GetUserTimeline(
         screen_name=screen_name,
         include_rts=False,
         exclude_replies=True,
         count=count
-    )).text
+    )
+
+def recent_keyword_tweet(screen_name:str, keyword:str, count:int = 200):
+    try:
+        return choice([tweet for tweet in recent_tweets(screen_name, count) if keyword in tweet.text])
+    except IndexError:
+        return
 
 client.run(key['DISCORD_TOKEN'])
